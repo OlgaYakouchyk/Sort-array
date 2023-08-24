23241a1b17d7d4ca0cc84adeed183a231bc5fed4
OlgaYakouchyk <109695976+OlgaYakouchyk@users.noreply.github.com>
2023-08-23 21:57:38 -0400

    same chenges

--- script.js
+++ script.js
@@ -6,15 +6,13 @@ const sameArray = ['hi', 'hello', 'world', 'Olga']
 
 button.addEventListener('click', function(){
     const list =document.createElement('ul')
-    const listItem =document.createElement('li')
+    
    
     
     wrapper.append(list)
     for(let i = 0; i<sameArray.length; i++){
-
-        const itemArr = sameArray[i]
-        list.textContent = itemArr
-
+        const listItem =document.createElement('li')
+        listItem.textContent =  sameArray[i]
         list.append(listItem)
 
     }
